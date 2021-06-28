df255f9b45387e7e3f6d4df2e6751e9b1b71207b
Norbert Sztyk <norbert2000@onet.eu>
2021-06-28 14:11:44 +0200

    Working and not bouncing timer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="bg-gray-600 min-h-screen text-center pt-24">
     <div class="w-1/3 mx-auto text-gray-200">
-      <span v-for="letter in textToObject" :key="letter.id" :class="letterClass(letter.state)">
+      <span
+        v-for="letter in textToObject"
+        :key="letter.id"
+        :class="letterClass(letter.state)"
+      >
         {{ letter.letter }}
       </span>
     </div>
@@ -10,13 +14,22 @@
       class="mt-10 bg-transparent outline-none text-gray-200 border-b-2 border-gray-200"
       v-model="typedText"
     />
+    <br>
+    <button @click="startTimer" class="text-gray-50 mr-10">Start</button>
+    <button @click="stopTimer" class="text-gray-50">Stop</button>
+    <br>
+    {{ time }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, watch } from "vue";
-import { getFirstUncompletedWorld} from "./use/speedTyperLogic";
-import { textToArray, textToObject, LetterState } from "./use/speedTyperInitGame";
+import { getFirstUncompletedWorld } from "./use/speedTyperLogic";
+import {
+  textToArray,
+  textToObject,
+  LetterState,
+} from "./use/speedTyperInitGame";
 import {
   handleUserInput,
   writtenText,
@@ -27,6 +40,8 @@ export default defineComponent({
   name: "App",
   setup() {
     const typedText = ref("");
+    const time = ref('0');
+    const interval = ref(0);
 
     watch(typedText, (value) => {
       typedText.value = handleUserInput(value);
@@ -35,10 +50,22 @@ export default defineComponent({
     const letterClass = (state: LetterState) => {
       switch (state) {
         case LetterState.Correct:
-          return "text-green-500"
+          return "text-green-500";
         case LetterState.Wrong:
-          return "text-gray-200 bg-red-500"
+          return "text-gray-200 bg-red-500";
       }
+    };
+
+    const incerementTime = () => {
+      time.value = (parseFloat(time.value) + 0.01).toFixed(2)
+    }
+
+    const startTimer = () => {
+      interval.value = setInterval(() => (incerementTime()), 10);
+    };
+
+    const stopTimer = () => {
+      clearInterval(interval.value)
     }
 
     return {
@@ -48,7 +75,10 @@ export default defineComponent({
       getFirstUncompletedWorld,
       writtenText,
       maxWordsTyped,
-      letterClass
+      letterClass,
+      time,
+      startTimer,
+      stopTimer,
     };
   },
 });
